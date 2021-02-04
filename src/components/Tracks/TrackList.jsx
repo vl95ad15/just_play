698aaf5c83eecd63368201c41e9cae809f11946d
Vlad <martsul.vlad@gmail.com>
2021-02-04 22:19:16 +0300

    add unique key for TrackListItems

--- src/components/Tracks/TrackList.jsx
+++ src/components/Tracks/TrackList.jsx
@@ -5,7 +5,7 @@ import './TrackList.css';
 function TrackList({ tracks }) {
   return(
     <div className="TrackList">
-      {tracks.map(track => <TrackListItem track={track} />)}
+      {tracks.map((track, index) => <TrackListItem track={track} key={index.toString()}/>)}
     </div>
   )
 }

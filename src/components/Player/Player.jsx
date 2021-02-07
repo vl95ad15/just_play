f9a97675977083e8860dfda4b4346928c336b261
Vlad <martsul.vlad@gmail.com>
2021-02-07 22:21:24 +0300

    new context player w/ styles

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,81 +1,128 @@
-import React, { useState, useRef, useEffect } from "react";
-import PlayerDetails from "./PlayerDetails";
-import PlayerControls from "./PlayerControls";
-import PlayerVolume from "./PlayerVolume"
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { Context } from "../../context/context";
 
 import "./Player.css";
 
-function Player(props) {
-  const audioEl = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  // const [volume, setVolume] = useState(0.5);
-  // const [duration, setDuration] = useState("00:00");
-  // const [position, setPosition] = useState(0);
+function Player() {
+  // Global State
+  const {
+    currentSong,
+    songs,
+    nextSong,
+    prevSong,
+    repeat,
+    random,
+    playing,
+    toggleRandom,
+    toggleRepeat,
+    togglePlaying,
+    handleEnd,
+  } = useContext(Context);
 
+  const audio = useRef("audio_tag");
 
-  const format = (n) => {
-    return (n - (n %= 60)) / 60 + (9 < n ? ":" : ":0") + ~~n;
+  // self State
+  const [statevolum, setStateVolum] = useState(0.3);
+  const [dur, setDur] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const fmtMSS = (s) => {
+    return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + ~~s;
   };
 
-  // useEffect(() => {
-  //   setVolume();
-  // }, [volume]);
+  const toggleAudio = () =>
+    audio.current.paused ? audio.current.play() : audio.current.pause();
+
+  const handleVolume = (q) => {
+    setStateVolum(q);
+    audio.current.volume = q;
+  };
+
+  const handleProgress = (e) => {
+    let compute = (e.target.value * dur) / 100;
+    setCurrentTime(compute);
+    audio.current.currentTime = compute;
+  };
 
   useEffect(() => {
-    if (isPlaying) {
-      audioEl.current.play();
-    } else {
-      audioEl.current.pause();
+    audio.current.volume = statevolum;
+    if (playing) {
+      toggleAudio();
     }
-  }, [isPlaying]);
-
-  // setDuration(audioEl.current.duration);
-
-  const setVol = (q) => {
-    audioEl.current.volume = q;
-  };
-
-  const SkipSong = (forwards = true) => {
-    if (forwards) {
-      props.setCurrentSongIndex(() => {
-        let temp = props.currentSongIndex;
-        temp++;
-
-        if (temp > props.songs.length - 1) {
-          temp = 0;
-        }
-        return temp;
-      });
-    } else {
-      props.setCurrentSongIndex(() => {
-        let temp = props.currentSongIndex;
-        temp--;
-
-        if (temp < 0) {
-          temp = props.songs.length - 1;
-        }
-        return temp;
-      });
-    }
-  };
+  }, [currentSong]);
 
   return (
     <div className="Player">
       <audio
-        src={props.songs[props.currentSongIndex].path}
-        ref={audioEl}
-      ></audio>
-      <PlayerDetails song={props.songs[props.currentSongIndex]} />
-      <PlayerControls
-        isPlaying={isPlaying}
-        setIsPlaying={setIsPlaying}
-        SkipSong={SkipSong}
-        
-        // setPosition={setPosition}
-        // duration={duration}
-        format={format}
+        onTimeUpdate={(e) => setCurrentTime(e.target.currentTime)}
+        onCanPlay={(e) => setDur(e.target.duration)}
+        onEnded={handleEnd}
+        ref={audio}
+        type="audio/mpeg"
+        preload="true"
+        src={songs[currentSong].path}
       />
-      <PlayerVolume setVolume={setVol} />
+      <div className="PlayerDetails">
+      <div className="DetailsImg">
+        <img src={songs[currentSong].image} alt="img" />
+        </div>
+        <div className="DetailsInfo">
+        <span className="DetailsTitle">{songs[currentSong].title}</span>
+        <span className="DetailsArtist">{songs[currentSong].artist}</span>
+        </div>
+      </div>
+      <div className="ControlsWrapper">
+        <div className="PlayerButtons">
+        <div
+          onClick={toggleRandom}
+          className={"random " + (random ? "active" : "")}
+        >
+          <i className="fas fa-random"></i>
+        </div>
+          <div className="PreviousSongButton" onClick={prevSong}>
+            <i className="fas fa-step-backward"></i>
+          </div>
+          <div
+            className="PlayButton"
+            onClick={() => {
+              togglePlaying();
+              toggleAudio();
+            }}
+          ><i className="fas fa-play fa-2x" />
+          </div>
+          <div className="NextSongButton" onClick={nextSong}>
+            <i className="fas fa-step-forward"></i>
+          </div>
+          <div
+          onClick={toggleRepeat}
+          className={"repeat " + (repeat ? "active" : "")}
+        >
+          <i className="fas fa-redo-alt"></i>
+        </div>
+        </div>
+        <div className="ProgressBarWrapper">
+          <div className="CurrentTime">{fmtMSS(currentTime)}</div>
+          <input
+            onChange={handleProgress}
+            value={dur ? (currentTime * 100) / dur : 0}
+            type="range"
+            name="progresBar"
+            className="ProgressBar"
+          />
+          <div className="Duration">{fmtMSS(dur)}</div>
+        </div>
+      </div>
+      <div className="VolumeSlider">
+          <i className="fas fa-volume-down"></i>
+        <input
+          value={Math.round(statevolum * 100)}
+          type="range"
+          name="volBar"
+          id="volBar"
+          onChange={(e) => handleVolume(e.target.value / 100)}
+        />
+      </div>
     </div>
   );
 }

bf555c12e944e34519504889cc931111f6defa2c
Vlad <martsul.vlad@gmail.com>
2021-02-07 22:18:10 +0300

    replace pages folder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Songs from '../components/Songs/Songs';
+import React from 'react';
+
+import './Page.css';
+
+function Home(props) {
+  return(
+      <main>
+        <Songs />
+      </main>
+  )
+}
+
+export default Home;
---
+++ src/pages/Library.jsx
@@ -0,0 +1,11 @@
+import './Page.css';
+
+function Library(props) {
+  return(
+    <main>
+      <p>Library Page</p>
+    </main>
+  )
+}
+
+export default Library;

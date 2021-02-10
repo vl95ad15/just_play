1a981dc8904ec48ac6d90e44e0e7386b455658f1
Vlad <martsul.vlad@gmail.com>
2021-02-10 16:00:09 +0300

    clear code

--- src/components/Buttons/PlayPause.jsx
+++ src/components/Buttons/PlayPause.jsx
@@ -3,23 +3,6 @@ import { Context } from '../../context/context'
 
 function PlayPause({ playing, songId, isCurrentSong, visible }) {
     
-  const { dispatch } = useContext(Context)
-  
-    if (isCurrentSong && playing) {
-      return (
-        <i
-          className="fa fa-pause"
-          onClick={() => dispatch({ type: 'PAUSE' })}
-        />
-      )
-    } else {
-      return (
-        <i
-          className="fa fa-play"
-          onClick={() => dispatch({ type: 'PLAY', songId })}
-        />
-      )
-    }
   }
 
 export default PlayPause
